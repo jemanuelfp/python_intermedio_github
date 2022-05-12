da28864eb2997d9d1cf4f7c5e289a3d77be3304b
jemanuelfp <jemanuelfp@gmail.com>
2022-05-11 19:24:47 -0500

    se agregan dos funciones de saludos nombre y completo

--- Clase 4/modulos/saludos.py
+++ Clase 4/modulos/saludos.py
@@ -1,5 +1,10 @@
 def saludos():
     print("Hola")
 
+def saludo_nombre(nombre):
+    print(f"Hola {nombre}")
+
+def saludo_nombre_completo(nombre, apellido_paterno, apellido_materno = ''):
+    print(f"Hola {nombre} {apellido_paterno} {apellido_materno}")
 
 nombre = "Emanuel de la Fuente"
