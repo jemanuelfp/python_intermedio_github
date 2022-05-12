61624cd3b4fd897a1f48e4a4c04217a910bd011e
jemanuelfp <jemanuelfp@gmail.com>
2022-05-11 21:02:15 -0500

    1.- case de carpetas

--- Clase 4/modulos/main.py
+++ Clase 4/modulos/main.py
@@ -11,11 +11,17 @@
 
 from saludos import *
 
-saludos()
-saludo_nombre("Vane")
-saludo_nombre_completo("Carlos", "Lopez")
+#saludos()
+#saludo_nombre("Vane")
+#saludo_nombre_completo("Carlos", "Lopez")
 
 # ejemplo de alias
 #from saludos_miguel import saludos as saludo_1
 #from saludos_sarai  import saludos as sadulo_2
 #
+
+from carpetas import crear_carpetas, elimina_carpetas
+
+crear_carpetas("pruebas_carpeta",3)
+
+elimina_carpetas("pruebas_carpeta",3)
---
+++ Clase 4/modulos/principal.py
@@ -0,0 +1,6 @@
+import random
+
+numero_aleatorio = random.randint(1,10)
+
+print(numero_aleatorio)
+
