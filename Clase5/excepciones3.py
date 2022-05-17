f57ba64573e5d2eb2e4837718f6ac1193f47763f
jemanuelfp <jemanuelfp@gmail.com>
2022-05-16 20:59:04 -0500

    creo una carpeta recursiva, par acrear multiples carpetas, con un consecutivo

--- Clase5/excepciones3.py
+++ Clase5/excepciones3.py
@@ -8,9 +8,22 @@ import os
 # carpeta(1)
 # carpeta(2)
 
-contenido = os.listdir("D:/200.- Python/python_intermedio_github")
+#contenido = os.listdir("D:/200.- Python/python_intermedio_github")
 
-print(contenido)
+#print(contenido)
 
 
+def crear_carpetas(nombre_de_la_carpeta, consecutivo = 0):
+    try:
+        if consecutivo < 1:
+            os.mkdir(nombre_de_la_carpeta)
+        else:
+            os.mkdir(nombre_de_la_carpeta + f'({consecutivo})')
+
+    except FileExistsError as ex:
+        crear_carpetas(nombre_de_la_carpeta, consecutivo + 1)
+
+
+
+crear_carpetas("funcion_recursiva")
 
