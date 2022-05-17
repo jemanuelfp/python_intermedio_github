60a88fa689b104c2f00314a92ed19d2534597553
jemanuelfp <jemanuelfp@gmail.com>
2022-05-16 20:34:38 -0500

    ejemplo de lista de directorios de carpetas

---
+++ Clase5/excepciones3.py
@@ -0,0 +1,16 @@
+# Actividad excepciones
+# Replicar el comportamiento de creacion de carpetas con windows
+
+import os
+
+# al crear un directorio si ya existe agregarle entre parantesis el consecutivo
+# carpeta
+# carpeta(1)
+# carpeta(2)
+
+contenido = os.listdir("D:/200.- Python/python_intermedio_github")
+
+print(contenido)
+
+
+
