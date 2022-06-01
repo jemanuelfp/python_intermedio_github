d3d2497032ee54d3f64453cb15d0e97a791bcbe6
jemanuelfp <jemanuelfp@gmail.com>
2022-06-01 18:53:13 -0500

    se ingresan la ultimas clases de python

--- Clase6/peticiones.py
+++ Clase6/peticiones.py
@@ -5,16 +5,15 @@ import os
 os.system('cls')
 
 
-json_resquest = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+json_resquest = requests.get("https://jsonplaceholder.typicode.com/users")
 
 #print(json_resquest)
 #print(type(json_resquest))
 
-print("=" * 50)
-print(json_resquest.text)
-print(type(json_resquest.text))
-print("=" * 50)
-print(json_resquest.json())
-print(type(json_resquest.json()))
+response_dic = json_resquest.json()
+
+for data in response_dic:
+    print(data)
+
 
 
